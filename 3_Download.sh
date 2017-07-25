512d2da18a738690441c7bed337c903be53ecc07
Katherine Halabi <katherineghalabi@gmail.com>
2017-07-25 13:23:50 -0400

    Create 3_Download.sh
    Script

---
+++ 3_Download.sh
@@ -0,0 +1,9 @@
+#bin/bash/
+
+wget -r -A ".png" https://jovemnerd.com.br
+wget -r -A ".jpeg" https://jovemnerd.com.br
+wget -r -A ".jpg" https://jovemnerd.com.br
+wget -r -A ".svg" https://jovemnerd.com.br
+wget -r -A ".gif" https://jovemnerd.com.br
+wget -r -A ".tiff" https://jovemnerd.com.br
+wget -r -A ".raw" https://jovemnerd.com.br
